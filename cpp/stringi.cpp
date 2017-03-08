7e9436cc3c9a8b5949dc6a41d8a04a12bf556964
kl2ag2 <kl2ag2@komp05.lo1cg.org>
2017-03-08 07:56:06 +0100

    szyfr_Vigenere'a

---
+++ cpp/stringi.cpp
@@ -0,0 +1,29 @@
+/*
+ * stringi.cpp
+ *
+ * Copyright 2017 user <user@lap79>
+ *
+ */
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    string tekst, szyfrogram, odszyfrowany;
+    cout << "Podaj tekst do zaszyfrowania: " << endl;
+    cin >> tekst;
+
+    cout << tekst << endl << "Rozmiar: " << tekst.size() << endl;
+    szyfrogram.resize(tekst.size());
+    for(unsigned int i = 0; i < tekst.size(); i++){
+        cout << tekst[i] << " ";
+        szyfrogram[i] = zakodowany_znak;
+    }
+
+	return 0;
+}
+

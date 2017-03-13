083bc35fa7ffabe3f212c6b8bcb8334fedbfa66c
Karol Kusik <karol@karol>
2017-03-13 20:20:32 +0100

    cezar cpp

--- cpp/Karol_Kusik_szyfr_cezara.cpp
+++ cpp/Karol_Kusik_szyfr_cezara.cpp
@@ -1,41 +1,43 @@
-
 #include <iostream>
-
+#include <cctype>
 using namespace std;
 
-void lower(char tekst[]) {
-// funkcja zamienia duże litery na małe
-    // użyj pętli do odczytywania kolejnych znaków
-    // sprawdź kod ASCII znaku
-    // jeżeli kod odpowiada dużej literze, podmień znak;
+
+void lower(char tekst[]){
+    if( isupper( tekst) )
+	{
+		t1 = tolower( tekst );
+		cout << t1 << endl;
+	}
+	else
+		cout << tekst << endl;
 }
 
-void szyfruj(char tekst[], int klucz){
+void szyfruj(char tekst[], int klucz) {
     int i = 0; // indeks
     klucz = klucz % 26;
-    while (tekst[i] != '\0') {
+    while (tekst[i] != '\0'){
         if ((int)tekst[i] + klucz > 122)
-            tekst[i] = (char)((int)tekst[i] + klucz - 26);
+            tekst[i] = (char)((int)tekst[i] + klucz-26);
         else
-            tekst[i] = (char)((int)tekst[i] + klucz);
-        i++;
-        // (int)tekst[i] - rzutowanie na kod ASCII
-        // (char)((int)tekst[i]) - zamiania kodu ASCII na znak
+             tekst[i] = (char)((int)tekst[i] + klucz);
+       // (int)tekst[i]
+       // ((char)((int)tekst[i])
+    i++;
     }
     cout << tekst;
 }
 
-int main(int argc, char **argv) {
+
+int main(int argc, char **argv){
     char tekst[100];
     int klucz = 3;
-    cout << "Podaj tekst do zaszyfrowania" << endl;
-    //cin >> tekst;
+    cout<< "Podaj tekst do zaszyfrowania: " << endl;
+    // cin >> tekst;
     cin.getline(tekst, 100);
     cout << "Podaj klucz: " << endl;
     cin >> klucz;
     szyfruj(tekst, klucz);
-
-
-	return 0;
+    lower(tekst);
+    return 0;
 }
-

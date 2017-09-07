dc8e803dd9ee512ca6b01f708f99e563fb371108
kl3ag2 <kl3ag2@komp05.lo1cg.org>
2017-09-07 08:40:19 +0200

    Sito Eratostenesa

--- klasa 3/hello.cpp
+++ klasa 3/hello.cpp
@@ -1,44 +1,73 @@
-/*
- * hello.cpp
- * 
- * Copyright 2017  <>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
-
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+
+
 #define ROK_TERAZ 2017
+#define ROK_CPP 1983
+
+void parzyste (int n)
+{
+    for(int i = 0; i <= n ; i=i+2)
+    {
+        cout << i << endl;
+    }
+    
+}
 
 int main(int argc, char **argv)
-{
+{/*
     char imie[20];
     int wiek;
+    int rok_urodzenia;
     cout << "Witaj! Imię użytkownika:" << endl;
     cin >> imie;
     cout << "Witaj, " << imie << "! Ile masz lat?" << endl;
+    rok_urodzenia = ROK_TERAZ-wiek;
     cin >> wiek;
     cout << "Urodziłeś się w " << ROK_TERAZ-wiek << " roku." << endl;
+    if(ROK_CPP < rok_urodzenia)
+    {
+        cout << "Jestem starszy." << endl;
+    }
+    else if (ROK_CPP > rok_urodzenia)
+    {
+        cout << "Jesteś starszy." << endl;
+    }
+    else
+    {
+        cout << "Mamy tyle samo lat." << endl;
+    }
+    int n;
+    cout << "Podaj liczbę naturalną: ";
+    cin >> n;
+    parzyste(n);
+    */
     
-	
+    int liczba, i, j, pierwiastek, *tablica;
+    cout << "Podaj maksimum: ";
+    cin >> liczba;
+     
+    pierwiastek = (int)sqrt((double)liczba);
+    tablica = new int[liczba+1];
+    for (i = 0; i <= liczba; i++)
+    tablica[i] = i;
+     
+    for (i = 2; i <= pierwiastek; i++)
+    {
+    if (tablica[i] != 0)
+    for (j = 2*i; j <= liczba; j+= i)
+    tablica[j] = 0;
+    }
+     
+    cout << "Liczby pierwsze z przedzialu <2," << liczba << "> to:\n";
+    for (i = 2; i <= liczba; i++)
+    if (tablica[i] != 0)
+    cout << tablica[i] << " ";
+    cout << endl;
+    
 	return 0;
 }
 
--- klasa 3/hello.py
+++ klasa 3/hello.py
@@ -2,33 +2,36 @@
 # -*- coding: utf-8 -*-
 #
 #  hello.py
-#  
-#  Copyright 2017  <>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+
 ROK_TERAZ = 2017
+ROK_PYTHON = 1991
+
+def parzyste(n):
+    ile = list(range(0, n+1, 2))
+    print(ile)
+    return len(ile)
+        
 
 def main(args):
     imie = input("Imię? ")
     print ("Witaj,", imie)
     wiek = int(input("Wiek? "))
+    rok_urodzenia = ROK_TERAZ - wiek
     print ("Urodziłeś się w", ROK_TERAZ - wiek, "roku.")
+    
+    if (ROK_PYTHON < rok_urodzenia):
+        print ("Jestem starszy.")
+    elif (ROK_PYTHON > rok_urodzenia):
+        print ("Jesteś starszy.")
+    else:
+        print ("Mamy tyle samo lat.")
+    
+    n = int(input("Podaj liczbę naturalną: "))
+    print("Ilość parzystych: ", parzyste(n))
+    
     return 0
+    
+    
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))

6ea9fd8b082658a6c769300c3882699c4e50d71a
kl3ag2 <kl3ag2@komp05.lo1cg.org>
2017-11-09 08:45:55 +0100

    orm

--- baza3ag2/orm/pracownicy_orm.py
+++ baza3ag2/orm/pracownicy_orm.py
@@ -4,12 +4,13 @@
 #  pracownicy_orm.py
 
 from peewee import *
+from dane import *
 
-baza_plik = SqliteDatabase(':memory:')
+baza_plik = SqliteDatabase('baza.db')
 
 class BaseModel(Model):
     class Meta:
-        baza = baza_plik
+        database = baza_plik
 
 
 class Premia(BaseModel):
@@ -28,14 +29,57 @@ class Pracownik(BaseModel):
     nazwisko = CharField()
     imie = CharField()
     stanowisko = ForeignKeyField(Premia)
-    data_zatr = DataField()
-    placa = DecimalField()
-    premia = DecimalField()
+    data_zatr = DateField()
+    placa = DecimalField(decimal_places=2)
     id_dzial = ForeignKeyField(Dzial)
-    
+    premia = DecimalField(decimal_places=2, default=0)
+
+
 baza_plik.connect() # połączenie z bazą
-baza_plik.create_tables([Premia, Dzial, Pracownik], True)
+#baza_plik.create_tables([Premia, Dzial, Pracownik], True)
+
+#obiekt = Premia(id='Kierowca', Premia=0.2) #utworzenie instancji klasy
+#~dane = [
+    #~{'id' : 'Kierowca', 'premia': '0.7'},
+    #~{'id' : 'Dyrektor', 'premia': '0.7'},
+    #~{'id' : 'Inżynier', 'premia': '0.4'}
+#~]
+#~for rekord in dane:
+#~Premia.create(id=rekord['id'], premia=dane['premia'])
+
+premia = dane_z_pliku('premia.txt')
+premia = wyczysc_dane(premia, 1)
+
+#~print(premia)
+#~print(Premia._meta.sorted_field_names)
+
+# UTWORZENIE LISTY SŁOWNIKÓW dla premii
+premia = [(dict(zip(Premia._meta.sorted_field_names, rekord))) for rekord in premia]
+
+# UTWORZENIE LISTY SŁOWNIKÓW dla pracowników
+pracownicy = dane_z_pliku('pracownicy.txt')
+pracownicy = wyczysc_dane(pracownicy, 5)
+pracownicy = [(dict(zip(Pracownik._meta.sorted_field_names, rekord))) for rekord in pracownicy]
+
+# UTWORZENIE LISTY SŁOWNIKÓW dla działów
+dzial = dane_z_pliku('dział.txt')
+dzial = [(dict(zip(Dzial._meta.sorted_field_names, rekord))) for rekord in dzial]
+
+#~print(premia)
+#~print(dzial)
+#~print(pracownicy)
+
+with baza_plik.atomic():
+    Premia.insert_many(premia).execute()
     
+with baza_plik.atomic():
+    Dzial.insert_many(dzial).execute()
+    
+with baza_plik.atomic():
+    Pracownik.insert_many(pracownicy).execute()
+
+baza_plik.commit() #zatwierdzenie operacji
+
 def main(args):
     
     return 0

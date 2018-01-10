47ea4d490ecc29962009db01bcc2f2b3e2d94580
kl3ag2 <karol22322@github.com>
2018-01-10 13:30:02 +0100

    cpp

--- cpp/tab_dynamiczne.cpp
+++ cpp/tab_dynamiczne.cpp
@@ -8,22 +8,62 @@
 
 using namespace std;
 
+void wprowadz(float *t, int ile) {
+    for(int i = 0; i < ile; i++) {
+        cout << "Podaj liczbę: " << endl;
+        // cin >> t[i];
+        cout << "Adres elementu: " << t + i << endl;
+        cin >> *(t + i);
+    }
+}
+
+void drukuj(float *t, int ile) {
+    for(int i = 0; i < ile; i++) {
+        cout << *(t + i);
+    }
+}
+
+int tab1W() {
+    cout << "Ile poda liczb? ";
+    
+    int ile;
+    cin >> ile;
+        
+    cout << sizeof(ile) << endl;
+        
+    try {
+        float *ptab = NULL;
+        ptab = new float[ile]; // utworzenie tablicy dynamicznej
+        wprowadz(ptab, ile);
+        drukuj(ptab, ile);
+    } catch(bad_alloc) {
+        cout << "Za mało pamięci!";
+        return 1;
+    }
+    return 0;
+}
+
+int tab2W() {
+    int w, k, i, j;
+    cout << "Podaj liczbę wierszy i kolumn: ";
+    cin >> w >> k;
+    int **tab; // deklaracja wskaźnika do wskaźnika
+        
+    try {
+        tab = new int *[w]; // utworzenie tablicy dynamicznej
+        wprowadz(ptab, ile);
+        drukuj(ptab, ile);
+    } catch(bad_alloc) {
+        cout << "Za mało pamięci!";
+        return 1;
+    }
+    return 0;
+}
+   
+
 int main(int argc, char **argv)
 {
-	tab1W() {
-        int *ptab = NULL;
-        cout << "Ile poda liczb? ";
-        int ile;
-        cin >> ile;
-        
-        try {
-            ptab = new int[ile]; // utworzenie tablicy dynamicznej
-        } catch(bad_alloc) {
-            cout << "Za mało pamięci!";
-            return 1;
-        }
-}
-    
+    tab1W();
     
 	return 0;
 }

08aee65ff3a337a312a2ec6f194ff8a35880b8fd
karol22322 <karol22322@gmail.com>
2018-01-23 19:23:28 +0100

    Euklides

--- cpp/ulamek.cpp
+++ cpp/ulamek.cpp
@@ -24,8 +24,16 @@ public:
     int get_m() {
         return mianownik;
     }
-    int skracaj()
-   ; 
+    int skracaj(int l, int m) {
+		while(m != 0)
+			return skracaj(m, l % m);
+ 
+		cout << "Skrócony ułamek: " << licznik/l << "/";
+		return mianownik/l;
+		
+   
+   
+	}
 }; // koniec definicji klasy
 
 void Ulamek::zapisz(int l, int m) {
@@ -50,17 +58,23 @@ Ulamek::Ulamek(int l, int m) {
 int main(int argc, char **argv)
 {   
     
-    Ulamek ul1(4, 5);
+    Ulamek ul1(27, 24);
     Ulamek ul2(1, 7);
-    //ul1.zapisz(4, 5);
+    
+    
+    //ul1.zapisz(4, 6);
     //ul2.zapisz(1, 7);
     cout << "1 ułamek: ";
     ul1.wypisz();
     cout << endl << "2 ułamek: ";
     ul2.wypisz();
     
-    cout << ul1.get_l() << endl;
+    cout << endl << ul1.get_l() << "/";
     cout << ul1.get_m() << endl;
+    
+    int licznik = ul1.get_l();
+    int mianownik = ul1.get_m();
+	cout << ul1.skracaj(licznik, mianownik);
 
 	return 0;
 }
